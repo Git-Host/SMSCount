8ff9c568d1c53075e37e957aafa2804df7220b70
David Ragones <dragones@dreamcommerce.co>
2012-09-26 00:44:11 -0700

    twilio integration 2

--- src/main/java/servlet/SMSCountServlet.java
+++ src/main/java/servlet/SMSCountServlet.java
@@ -1,15 +1,25 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/*
 import com.twilio.sdk.verbs.TwiMLResponse;
 import com.twilio.sdk.verbs.TwiMLException;
 import com.twilio.sdk.verbs.Sms;
+*/
+
+import com.twilio.sdk.resource.instance.Account;
+import com.twilio.sdk.TwilioRestClient;
+import com.twilio.sdk.TwilioRestException;
+import com.twilio.sdk.resource.factory.SmsFactory;
+import com.twilio.sdk.resource.instance.Sms;
 
 @WebServlet(
         name = "SMSCount", 
@@ -17,6 +27,29 @@ import com.twilio.sdk.verbs.Sms;
     )
 public class SMSCountServlet extends HttpServlet {
 
+	private static final long serialVersionUID = 1L;
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException 
+	{
+		try
+		{
+			TwilioRestClient client = new TwilioRestClient("AC6f8edababe938c819e2bf19a9ca6548b", "114795015624e7a9036e9dd2dd50b07f");
+
+			Account account = client.getAccount();
+
+			SmsFactory smsFactory = account.getSmsFactory();
+			Map<String,String> smsParams = new HashMap<String,String>();
+			smsParams.put("To", "+14152442957"); 
+			smsParams.put("From", "(415) 373-6437");
+			smsParams.put("Body", "Where's Wallace?");
+			Sms sms = smsFactory.create(smsParams);
+		}
+		catch(TwilioRestException e)
+		{
+
+		}
+	}
+	/*
     public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
         TwiMLResponse twiml = new TwiMLResponse();
         Sms sms = new Sms("Hello, Mobile Monkey");
@@ -29,5 +62,6 @@ public class SMSCountServlet extends HttpServlet {
         response.setContentType("application/xml");
         response.getWriter().print(twiml.toXML());
     }
+    */
     
 }
